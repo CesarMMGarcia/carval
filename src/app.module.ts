b73c8d0ca8aebca9bb76314bf1476ff382099714
Cesar Garcia <cesargarcia@Cesars-MacBook-Pro-2.local>
2024-09-27 23:55:40 -0700

    removed password from getUser response

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [  TypeOrmModule.forRoot({
+  imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [User, Report],
     synchronize: true,
-  }),ReportsModule, UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  }), ReportsModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
